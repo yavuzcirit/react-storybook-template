3c21b0d1c265436268038956a09ef1f0253a60ed
Yavuz Cirit <yavuzcirit23@gmail.com>
2022-01-28 16:50:47 +0300

    stories in stories added

---
+++ src/Subscription/Subscription.stories.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Primary } from "../components/Button/Button.stories"
+import { Large } from "../components/Input/Input.stories"
+
+export default {
+  title:"Form/Subscription"
+}
+
+export const PrimarySubscription = () => (
+  <>
+  <Large />
+  <Primary />
+  </>
+)
