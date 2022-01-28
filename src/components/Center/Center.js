c15ab53f1b2f6b0785352d2376df9c28af67feb4
Yavuz Cirit <yavuzcirit23@gmail.com>
2022-01-28 17:50:46 +0300

    decorator added

---
+++ src/components/Center/Center.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Center.css"
+
+function Center(props) {
+  return (
+  <div className='center'>
+    {props.children}
+  </div>
+  );
+}
+
+export default Center;
